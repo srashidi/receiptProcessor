1cb409bde7bd8a3150ea3ea97d83edc9a57dba30
Shaunty Rashidi <srashidi@users.noreply.github.com>
2024-12-02 12:08:16 -0800

    Create method signatures for API paths

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from uuid import uuid4
 
 app = FastAPI()
+receipts = {}
 
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to My Receipt Processor"}
+
+@app.post("/receipts/process")
+async def process_receipt(receipt: dict):
+    receipt_id = uuid4()
+    receipts[receipt_id] = receipt
+    return receipt_id
+
+@app.get("/receipts/{receipt_id}.points")
+async def get_receipt_points(receipt_id: str):
+    pass
